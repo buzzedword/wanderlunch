64138871689879354bd2ab9099c29128a1218948
Daniel Garcia <danny@noisenewyork.com>
2013-08-01 16:54:32 -0400

    Wanderlunch.

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+'use strict';
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+
+    jshint: {
+      gruntfile: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: 'Gruntfile.js'
+      },
+      src: {
+        options: {
+          jshintrc: 'assets/js/.jshintrc'
+        },
+        src: ['assets/js/**/*.js']
+      },
+    },
+
+    requirejs: {
+      compile: {
+        options: {
+          name: "main",
+          baseUrl: "assets/js",
+          out: "assets/js/prod_build.js",
+          optimize: "uglify2",
+          paths: {
+            jquery: "../libs/jquery",
+          },
+        }
+      }
+    },
+
+    less: {
+      dev: {
+        files: {
+          "assets/css/style.css": "assets/less/index.less"
+        }
+      },
+      production: {
+        options: {
+          yuicompress: true
+        },
+        files: {
+          "assets/css/style.css": "assets/less/index.less"
+        }
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true,
+      },
+      src: {
+        files: ['assets/js/**/*.js', 'assets/less/**/*.less','*.html', 'Gruntfile.js'],
+        tasks: ['default'],
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: '.'
+        }
+      }
+    },
+
+    open: {
+      server: {
+        url: 'http://localhost:<%= connect.server.options.port %>'
+      }
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-devtools');
+
+  // Tasks definition.
+  grunt.registerTask('default', ['jshint:gruntfile', 'jshint:src', 'less:dev' ]);
+  grunt.registerTask('build', ['jshint:gruntfile', 'jshint:src', 'requirejs', 'less:production' ]);
+  grunt.registerTask('server', ['connect', 'open', 'watch' ]);
+
+};
---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+require.config({
+  baseUrl: 'assets/libs',
+  paths: {
+    jquery: 'jquery'
+  }
+});
+require(["jquery"],function($){
+  var directions = ["north", "south", "east", "west"];
+  var trains = ["1", "6", "A", "F", "N"];
+  var stops = [1,2,3,4,5,6,7];
+
+  function randomize(set){
+    return set[Math.floor(Math.random()*set.length)];
+  }
+
+  function trainName(name){
+    return "images/" + name + "-train.png";
+  }
+
+  function pluralize(val){
+    return ((val > 1)? 'stops' : 'stop');
+  }
+
+  function roll(){
+    var direction = randomize(directions);
+    var train = trainName(randomize(trains));
+    var stop = randomize(stops);
+    var plural = pluralize(stop);
+
+    $('.direction').text(direction);
+    $('.line').attr('src', train);
+    $('.stops').text(stop);
+
+    $('.pluralize').text(plural);
+  }
+
+  $(function(){
+    roll();
+
+    $('.wanderlunch').on('click', roll);
+  });
+});
